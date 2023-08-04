67cf9e061be9ba17b5635c4e6f7e5fc55b12332c
João Henrique <ribeiro95joao@gmail.com>
2023-08-03 21:05:52 -0300

    f

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import entities.Product;
 
+import java.io.*;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +21,44 @@ public class Program {
         System.out.println("Enter file path: ");
         String sourceFileStr = sc.nextLine();
 
-        
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
+
+        boolean success = new File(sourceFileStr + "\\out").mkdir();
+
+        String targetFileStr = sourceFolderStr + "//out//summary.csv";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
+
+            String itemCsv = br.readLine();
+            while (itemCsv != null){
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+                for(Product item : list){
+                    bw.write(item.getName() + ", " + String.format("%.2f", item.total()));
+                    bw.newLine();
+                }
+
+                System.out.println(targetFileStr + " CREATED!");
+
+            }
+            catch (IOException e){
+                System.out.println("Error writing file: " + e.getMessage());
+            }
+
+        }
+        catch(IOException e){
+            System.out.println("Error writing file: " +e.getMessage());
+        }
 
 
         sc.close();
